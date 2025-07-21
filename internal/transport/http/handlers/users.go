146652c68b61b8ff3931473da8c3999261752dbd
Coeeter <nasrullah01n@gmail.com>
2025-07-21 22:44:51 +0800

    Add users HTTP handler with create user endpoint

---
+++ internal/transport/http/handlers/users.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/coeeter/aniways/internal/service/users"
+	"github.com/go-chi/chi/v5"
+)
+
+func MountUsersRoutes(r chi.Router, userService *users.UserService) {
+	r.Route("/users", func(r chi.Router) {
+		r.Put("/", createUser(userService))
+	})
+}
+
+func createUser(userService *users.UserService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := &users.CreateUser{}
+		if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		u, err := userService.CreateUser(r.Context(), user.Username, user.Email, user.Password)
+
+		if err != nil {
+			log.Printf("Error creating user: %v", err)
+			switch err {
+			case users.ErrEmailTaken, users.ErrUsernameTaken:
+				jsonError(w, http.StatusConflict, err.Error())
+			case users.ErrPasswordTooLong:
+				jsonError(w, http.StatusBadRequest, err.Error())
+			default:
+				jsonError(w, http.StatusInternalServerError, err.Error())
+			}
+		}
+
+		jsonOK(w, u)
+	}
+}
