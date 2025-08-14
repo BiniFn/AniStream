5456a235f3465580f82e94ef97daddae32675db1
Coeeter <nasrullah01n@gmail.com>
2025-08-15 01:06:58 +0800

    Made dark mode

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<script>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	let number = $state(0);
 </script>
+
+<Button onclick={() => number++}>
+	Increment {number}
+</Button>
